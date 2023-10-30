87f8bd2f40edb89b8db44924b86b7c994ff42396
AutoJunjie <38706868+AutoJunjie@users.noreply.github.com>
2023-10-30 22:38:00 +0800

    fix chinese_text_splitter.py

--- lambda/langchain_processor_dataload/chinese_text_splitter.py
+++ lambda/langchain_processor_dataload/chinese_text_splitter.py
@@ -1,124 +1,25 @@
-import os
-import json
-import traceback
-import urllib.parse
-import boto3
-from datetime import datetime
-import time
-from smart_search_dataload import SmartSearchDataload
+import re
+from typing import List
 
-EMBEDDING_ENDPOINT_NAME = os.environ.get('embedding_endpoint_name')
-host =  os.environ.get('host')
-index =  os.environ.get('index')
-region = os.environ.get('AWS_REGION')
-language = os.environ.get('language')
-port = 443
-bulk_size = 10000000
+from langchain.text_splitter import CharacterTextSplitter
 
-# retrieve secret manager value by key using boto3                                             
-sm_client = boto3.client('secretsmanager')
-master_user = sm_client.get_secret_value(SecretId='opensearch-master-user')['SecretString']
-data= json.loads(master_user)
-username = data.get('username')
-password = data.get('password')
 
-s3_res = boto3.resource('s3')
-s3_cli = boto3.client('s3')
+class ChineseTextSplitter(CharacterTextSplitter):
+    def __init__(self, pdf: bool = False, **kwargs):
+        super().__init__(**kwargs)
+        self.pdf = pdf
 
-def lambda_handler(event, context):
-    
-    print("event:",event)
-    print("host:",host)
-    print("region:",region)
-    print("language:",language)
-    print("username:",username)
-    print("password:",password)
-    
-    #try:
-    #    dataload = SmartSearchDataload()
-    #    dataload.init_cfg(index,
-    #                     username,
-    #                     password,
-    #                     host,
-    #                     port,
-    #                     EMBEDDING_ENDPOINT_NAME,
-    #                     region,
-    #                     language=language
-    #                     )
-    #                 
-
-    #    bucket_name = event['Records'][0]['s3']['bucket']['name']
-    #    file_name = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    #    local_file = "{}/{}".format('/tmp', file_name.split("/")[-1])
-    #
-    #    print("bucket_name:",bucket_name)
-    #    print("file_name:",file_name)
-    #    print("local_file:",local_file)
-
-    try:
-        bucket_name = event['Records'][0]['s3']['bucket']['name']
-        file_name = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-        local_file = "{}/{}".format('/tmp', file_name.split("/")[-1])
-        index =  os.environ.get('index')
-        
-        if len(file_name.split("/")[-2]) > 3:
-            index = file_name.split("/")[-2]
-            
-        print("bucket_name:",bucket_name)
-        print("file_name:",file_name)
-        print("index:",index)
-        print("local_file:",local_file)
-
-        dataload = SmartSearchDataload()
-        dataload.init_cfg(index,
-                         username,
-                         password,
-                         host,
-                         port,
-                         EMBEDDING_ENDPOINT_NAME,
-                         region,
-                         language=language
-                         )
-    
-        size = int(event['Records'][0]['s3']['object']['size'])
-        loaded_files = []
-        if size > 0:    
-            s3_cli.download_file(Bucket=bucket_name,
-                             Key=file_name,
-                             Filename=local_file
-                             )
-            print("finish download file")
-        
-            now1 = datetime.now()#begin time
-            loaded_files = dataload.init_knowledge_vector(local_file,bulk_size)
-            now2 = datetime.now()#endtime
-            print("File import takes time:",now2-now1)
-            print("Complete the import of the following documents:", str(loaded_files))
-        
-        else:
-            print("Empty file")
-        
-        response = {
-            "statusCode": 200,
-            "headers": {
-                    "Access-Control-Allow-Origin": '*'
-                },
-                "isBase64Encoded": False
-        }
-            
-        response['body'] = json.dumps(
-        {
-            'datetime':time.time(),
-            'loaded_files': loaded_files
-            
-        })
-        
-        print("response:",response)
-        return response
-        
-    except Exception as e:
-        traceback.print_exc()
-        return {
-            'statusCode': 400,
-            'body': str(e)
-        }
+    def split_text(self, text: str) -> List[str]:
+        if self.pdf:
+            text = re.sub(r"\n{3,}", "\n", text)
+            text = re.sub('\s', ' ', text)
+            text = text.replace("\n\n", "。")
+        sent_sep_pattern = re.compile(
+            '([﹒﹔﹖﹗．。！？]["’”」』]{0,2}|(?=["‘“「『]{1,2}|$))') 
+        sent_list = []
+        for ele in sent_sep_pattern.split(text):
+            if sent_sep_pattern.match(ele) and sent_list:
+                sent_list[-1] += ele
+            elif ele:
+                sent_list.append(ele)
+        return sent_list
