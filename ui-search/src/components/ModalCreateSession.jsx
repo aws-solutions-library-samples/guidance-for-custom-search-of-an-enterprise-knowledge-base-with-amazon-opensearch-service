2fbafa13b49eb118b837d46ef93d1bc3c86f5fed
Peiwen Li <impw@amazon.com>
2024-03-15 15:36:27 +0800

    ui-refactor: number of doc between 1 and 200

--- ui-search/src/components/ModalCreateSession.jsx
+++ ui-search/src/components/ModalCreateSession.jsx
@@ -541,11 +541,11 @@ export default function ModalCreateSession({ dismissModal, modalVisible }) {
                       <FormField
                         stretch
                         label="Number of doc for vector search"
-                        constraintText="Integer between 1 and 10"
+                        constraintText="Integer between 1 and 200"
                         errorText={
-                          topK >= 0 && topK <= 10
+                          topK >= 1 && topK <= 10
                             ? ''
-                            : 'A number between 0 and 10'
+                            : 'A number between 1 and 200'
                         }
                       >
                         <Input
@@ -564,11 +564,11 @@ export default function ModalCreateSession({ dismissModal, modalVisible }) {
                       <FormField
                         stretch
                         label="Number of doc for text search"
-                        constraintText="Integer between 1 and 10"
+                        constraintText="Integer between 1 and 200"
                         errorText={
-                          txtDocsNum >= 0 && txtDocsNum <= 10
+                          txtDocsNum >= 1 && txtDocsNum <= 200
                             ? ''
-                            : 'A number between 0 and 10'
+                            : 'A number between 1 and 200'
                         }
                       >
                         <Input
